c7c00999c2ef1eebb61353bafc2f9821cd3c61ea
Jordan Gonzalez <Jordanaigonzalez@gmail.com>
2024-08-21 11:41:20 -0400

    trie logic operates as per test module

--- Trielogic.py
+++ Trielogic.py
@@ -1,53 +1,60 @@
 import nltk
 import os
 
-
 # Class for Trie node
 class TrieNode:
-    # initializer
-    def __init__(self, char):
+     # initializer
+    def __init__(self,  char =''):
         self.char = char
         self.children ={}
         self.is_word = False
-
-
+       
 # Class for Trie structure
 class Trie(object):
-    # Initializer
+     # Initializer
     def __init__(self):
         self.root = TrieNode()
+  
        
-
-    # Inserts char into trie
+     # Inserts char into trie
     def insertchar(self, word):
         current = self.root
+       
         for i, char in enumerate(word):
             if char not in current.children:
                 prefix = word[0:i+1]
-                current.children[char]= TrieNode(prefix)
+                current.children[char] = TrieNode(prefix)
+               
             current = current.children[char]
         current.is_word = True
-
+        return True  
+        
+        
 
         # Finds char in trie
     def findchar(self, word):
         current = self.root
+      
         for char in word:
             if char not in current.children:
                 return False
-            current.children[char]= current
-        current.is_word = True
+            current = current.children[char]
+       
+        return current.is_word
+       
 
 
      #looks for prefix in node
     def startswith(self, prefix):
         current = self.root
-
+        
         for char in prefix:
             if char not in current.children:
                 return False
-            current= current.children[char]
+            current = current.children[char]
         return True
+    
+    
     
 
 
--- test_trielogic.py
+++ test_trielogic.py
@@ -1,33 +1,52 @@
 import unittest
-from Trielogic import TrieNode, Trie
+from trielogic import Trie
+import logging
+
+# Logging config
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Testtrie(unittest.TestCase):
-     # Test Trie node
-    def test_trienode(self):
-        self.trienode = TrieNode()
-   
-   
+    
      # Test trie structure
-    def test_trie(self):
-        self.trie = Trie()
-
+    def setUp(self):
+        
+        try :
+            self.trie = Trie()
+            logging.debug("Inializing Trie structure")
+       
+        except TypeError as e:
+            logging.debug(f"Type Error just occured: {e}")
+        
      # Test insert method
-    def test_insert(self):
+    def test_insert(self):     
         self.trie.insertchar("Hello") 
-        self.assertTrue(self.trie.findchar("hello"))
-        self.assertFalse(self.trie.findchar("hell"))
-        self.assertFalse(self.trie.findchar("helloo"))
+        self.assertTrue(self.trie.findchar("Hello"))
+        self.assertFalse(self.trie.findchar("Hell"))
+        self.assertFalse(self.trie.findchar("Helloo"))
         self.assertFalse(self.trie.findchar("world"))
+           
 
 
      # Test start with method
-    def test_startswith(self):
+    def test_startswith(self):       
         self.trie.insertchar("Hello")
-        self.assertTrue(self.trie.startswith("he"))
-        self.assertTrue(self.trie.startswith("hello"))
-        self.assertFalse(self.trie.startswith("helloo"))
+        self.assertTrue(self.trie.startswith("He"))
+        self.assertTrue(self.trie.startswith("Hell"))
+        self.assertTrue(self.trie.startswith("Hello"))
+        self.assertFalse(self.trie.startswith("Helloo"))
         self.assertFalse(self.trie.startswith("world"))
+        
+     
+     #Test find char method
+    def test_findchar(self):
+        self.trie.insertchar("Hello")
+        self.assertTrue(self.trie.findchar("Hello"))
+        self.assertFalse(self.trie.findchar("Helloo"))
+        self.assertFalse(self.trie.findchar("world"))
+        
+
+
     
        
 
